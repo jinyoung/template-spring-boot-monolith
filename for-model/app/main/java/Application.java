4c188e9c51b704887274097f3b8e7386e81572f8
Jinyoung Jang <jyjang@uengine.org>
2022-12-09 09:59:33 +0000

    why undefined?

--- for-model/app/main/java/Application.java
+++ for-model/app/main/java/Application.java
@@ -1,1 +1,17 @@
-undefined
+forEach: Model
+fileName: {{namePascalCase}}Application.java
+path: {{path}}
+---
+package {{options.package}};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class {{namePascalCase}}Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run({{namePascalCase}}Application.class, args);
+  }
+
+}
